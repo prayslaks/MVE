c2069b1d324a571aa89e299f0e69db36df93095b
prayslaks <prayslaks@naver.com>
2025-11-13 12:01:41 +0900

    2025-11-13 : 최초 커밋

---
+++ Source/MVE/MVE.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MVE : ModuleRules
+{
+	public MVE(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MVE",
+			"MVE/Variant_Platforming",
+			"MVE/Variant_Platforming/Animation",
+			"MVE/Variant_Combat",
+			"MVE/Variant_Combat/AI",
+			"MVE/Variant_Combat/Animation",
+			"MVE/Variant_Combat/Gameplay",
+			"MVE/Variant_Combat/Interfaces",
+			"MVE/Variant_Combat/UI",
+			"MVE/Variant_SideScrolling",
+			"MVE/Variant_SideScrolling/AI",
+			"MVE/Variant_SideScrolling/Gameplay",
+			"MVE/Variant_SideScrolling/Interfaces",
+			"MVE/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MVE/MVEGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MVEGameMode.h"
+
+AMVEGameMode::AMVEGameMode()
+{
+	// stub
+}
