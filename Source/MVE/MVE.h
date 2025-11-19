822e3fb52a3674e44187921a0a23774305513377
HyeseonLikesPizza <1725f0701@gmail.com>
2025-11-20 03:08:36 +0900

    [Feat] 로그 매크로 추가

--- Source/MVE/MVE.h
+++ Source/MVE/MVE.h
@@ -1,8 +1,17 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 
-/** Main log category used across the project */
+
 DECLARE_LOG_CATEGORY_EXTERN(LogMVE, Log, All);
+
+// 어디서 호출되는지 볼 수 있는 함수
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+
+// CALLINFO 매크로 편하게 로그찍는 매크로
+#define PRINTINFO() UE_LOG(LogMVE, Warning, TEXT("%s"), *CALLINFO)
+
+// 이거 쓰셈
+#define PRINTLOG(fmt, ...) \
+UE_LOG(LogMVE, Warning, TEXT("%s : %s"), *CALLINFO, *FString::Printf(fmt, ##__VA_ARGS__))
