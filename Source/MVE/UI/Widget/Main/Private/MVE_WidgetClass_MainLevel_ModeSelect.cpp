7d23daa4ea081c4368acd72c28b5e67514f73a6c
HyeseonLikesPizza <1725f0701@gmail.com>
2025-11-20 04:18:18 +0900

    [Feat] MainLevel 위젯 생성
    틀만 잡아놨고 동작 구현은 오늘 할거임

---
+++ Source/MVE/UI/Widget/Main/Private/MVE_WidgetClass_MainLevel_ModeSelect.cpp
@@ -0,0 +1,2 @@
+﻿
+#include "../Public/MVE_WidgetClass_MainLevel_ModeSelect.h"
---
+++ Source/MVE/UI/Widget/Main/Public/MVE_WidgetClass_MainLevel.h
@@ -0,0 +1,24 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MVE_WidgetClass_MainLevel.generated.h"
+
+class UButton;
+
+UCLASS()
+class MVE_API UMVE_WidgetClass_MainLevel : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ReturnDesktopButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> LoginButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CreditButton;
+};
