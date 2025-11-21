b6fbf992a935d7399e8c24782965dd4d421242ba
HyeseonLikesPizza <b1327e@naver.com>
2025-11-21 20:09:12 +0900

    [Feat] 프레임워크 관련 클래스 생성 / Main Widget 출력

---
+++ Source/MVE/Framework/Public/MVE_PC_Master.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MVE_PC_Master.generated.h"
+
+class UMVE_WidgetClass_MainLevel;
+
+UCLASS()
+class MVE_API AMVE_PC_Master : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVE_WidgetClass_MainLevel> MainWidgetClass;
+};
