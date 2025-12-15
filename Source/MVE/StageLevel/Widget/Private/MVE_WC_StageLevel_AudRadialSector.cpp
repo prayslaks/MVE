9a15f4de22d9505be55716703c320d1df1ce56a6
HyeseonLikesPizza <1725f0701@gmail.com>
2025-12-15 10:29:45 +0900

    [Fix] 병합 시 빌드 문제 해결
    - 폴더 이동 처리했던 스테이지 레벨 게임 모드 cpp가 남아있어서 삭제
    - 래디얼 섹터 위젯 cpp에 헤더 인클루드가 잘못 작성되어 있어서 수정

--- Source/MVE/StageLevel/Widget/Private/MVE_WC_StageLevel_AudRadialSector.cpp
+++ Source/MVE/StageLevel/Widget/Private/MVE_WC_StageLevel_AudRadialSector.cpp
@@ -1,3 +1,4 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
 
-#include "MVE_WC_StageLevel_AudRadialSector.generated.h"
+#include "StageLevel/Widget/Public/MVE_WC_StageLevel_AudRadialSector.h"
