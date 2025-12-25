f3d4df49813619d58552f41a637cd52edbd0e1da
prayslaks <prayslaks@naver.com>
2025-12-26 02:44:21 +0900

    [Feat] 시퀀스 액터 및 스포트라이트 시퀀스 시스템 구조 구축

---
+++ Source/MVE/StageLevel/Actor/Public/MVE_StageLevel_SequenceActorBase.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MVE_StageLevel_SequenceActorBase.generated.h"
+
+UCLASS()
+class MVE_API AMVE_StageLevel_SequenceActorBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMVE_StageLevel_SequenceActorBase();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 SequenceOrder;
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void ExecuteSequence(int32 SequenceNumber);
+};
---
+++ Source/MVE/StageLevel/Actor/Public/MVE_StageLevel_Spotlight.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVE_StageLevel_SequenceActorBase.h"
+#include "GameFramework/Actor.h"
+#include "MVE_StageLevel_Spotlight.generated.h"
+
+UCLASS()
+class MVE_API AMVE_StageLevel_Spotlight : public AMVE_StageLevel_SequenceActorBase
+{
+	GENERATED_BODY()
+	
+public:
+	AMVE_StageLevel_Spotlight();
+};
